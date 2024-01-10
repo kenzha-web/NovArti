c8ff2a7d68afb123ef19c90b79b7ab7004e5e360
Test Test <kenzhakhmetov01@gmail.com>
2024-01-10 23:58:49 +0600

    addendum

---
+++ src/app/App.tsx
@@ -0,0 +1,18 @@
+import './style/index.scss'
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
+
+const App = () => {
+	const {theme} = useTheme()
+	
+	return (
+		<div className={classNames('app', {}, [theme])}>
+			<Navbar />
+			<AppRouter />
+		</div>
+	);
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {render} from "react-dom";
-import App from "./App";
 import {BrowserRouter} from "react-router-dom";
-import ThemeProvider from "./theme/ThemeProvider";
+import App from "app/App";
+import {ThemeProvider} from "app/providers/ThemeProvider";
+
+console.log('test')
 
 render (
 	<BrowserRouter>
